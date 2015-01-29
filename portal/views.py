9c34998777dca8ba9c7b9432ec91bc37901b0c61
paulo <paulo@photoplay.com.br>
2015-01-29 16:07:21 -0200

    adicionando formulário de contato

--- portal/views.py
+++ portal/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, TemplateView, DetailView, FormView
+from forms import ContactForm
 
 # Create your views here.
 
@@ -13,3 +14,9 @@ class PortalProdutosView(TemplateView):
 
 class PortalContatoView(TemplateView):
 	template_name = "contato.html"
+
+	def get_context_data(self, **kwargs):
+		context = super(PortalContatoView, self).get_context_data(**kwargs)
+		context['contact_form'] = ContactForm()
+
+		return context
