31e596d3e9e97ebef71d6d4ec87bd0572edc374e
paulo <paulo@photoplay.com.br>
2015-03-10 13:02:48 -0300

    adicionando novo app: blog

--- photoplay/urls.py
+++ photoplay/urls.py
@@ -1,12 +1,16 @@
-from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
 from filebrowser.sites import site
-admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = i18n_patterns('',
     url(r'^admin/filebrowser/', include(site.urls)),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
     url(r'^municipios_app/', include('municipios.urls')),
     url(r'^', include('portal.urls')),
-)
+    url(r'^', include('blog.urls')),
+    url(r'^', include('cms.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
