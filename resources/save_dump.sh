0e4e2618ca41fabb55562a65921fd08f43636594
paulo <paulo@photoplay.com.br>
2015-03-11 16:44:30 -0300

    corrindo bug do link da imagem do blog

--- resources/save_dump.sh
+++ resources/save_dump.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py dumpdata portal municipios | python -m json.tool > /home/photo/apps/photoplay/photoplay/fixtures/initial_data.json
+python manage.py dumpdata portal municipios blog zinnia  | python -m json.tool > /home/photo/apps/photoplay/photoplay/fixtures/initial_data.json
