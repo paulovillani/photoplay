48ae6b4449526e3a1cae9ccc46992a85e481e87f
paulo.villani <paulo.villani@gmail.com>
2015-07-09 01:08:26 +0000

    arrumando resources no servidor, adicionando ao gitignore para manter cada um como deve ser e commitando dump do bd para correção do modelo

--- resources/save_dump.sh
+++ resources/save_dump.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python manage.py dumpdata portal municipios blog zinnia  | python -m json.tool > /home/photo/apps/photoplay/photoplay/fixtures/initial_data.json
+python2.7 manage.py dumpdata portal municipios blog zinnia  | python -m json.tool > /home/photosite/webapps/photoplay2/photoplay/photoplay/fixtures/initial_data.json
