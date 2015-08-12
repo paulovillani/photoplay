6722065d009aec501ff7262fb61b3d59f31f49d5
paulo.villani <paulo.villani@gmail.com>
2015-08-12 21:24:40 +0000

    mudando script de dump para incluir os modelos do publicador e os usuários cadastrados no sistema

--- resources/save_dump.sh
+++ resources/save_dump.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python2.7 manage.py dumpdata portal municipios blog zinnia  | python -m json.tool > /home/photosite/webapps/photoplay2/photoplay/photoplay/fixtures/initial_data.json
+python2.7 manage.py dumpdata portal municipios blog zinnia publicador auth.User | python -m json.tool > /home/photosite/webapps/photoplay2/photoplay/photoplay/fixtures/initial_data.json
