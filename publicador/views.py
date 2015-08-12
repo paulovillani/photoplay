ddc5c2bdb7c0fe45cb27811f899f735123a7efcd
paulo <paulo@photoplay.com.br>
2015-08-12 13:05:37 -0300

    commitando correção na view

--- publicador/views.py
+++ publicador/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, render_to_response
 from django.template.context import RequestContext
 from django.contrib.auth.decorators import login_required
 
-@login_required
+
 def home(request):
 	context = RequestContext(request,
 	                        {'request': request,
